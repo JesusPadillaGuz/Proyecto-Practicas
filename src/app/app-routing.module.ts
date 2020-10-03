c3af1ec6137461efd9d0df4c21d68afa514e394d
unknown <chuy_student23@outlook.com>
2020-10-02 21:35:33 -0500

    commit terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
+import { ComentariosComponent } from './comentarios/comentarios.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import {UsuariosComponent} from './usuarios/usuarios.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{ path: 'posts' , component: PostsComponent},
+{ path: 'usuarios' , component: UsuariosComponent},
+{ path: 'usuarios/:id' , component: UsuariosComponent},
+{ path: 'comentarios/:id' , component: ComentariosComponent},
+{ path: '**', pathMatch: 'full', redirectTo: 'posts'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { ServicioService } from './servicio.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { ComentariosComponent } from './comentarios/comentarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostsComponent,
+    UsuariosComponent,
+    ComentariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ServicioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
